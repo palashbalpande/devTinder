824b25daea01bbc17833a010556cbfd20aa9dd70
palashbalpande <palashbalpande37@gmail.com>
2024-09-22 23:10:52 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,24 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send("HAHAHAHAHAH");
-});
-
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Palash", lastName: "Balpande" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data successfully added");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Data successfully deleted");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from the Server!");
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+    res.send({ firstName: "Palash", lastName: "Balpande" });
 });
 
 app.listen(7777, () => {
