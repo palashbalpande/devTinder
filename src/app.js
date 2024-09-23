70c3be5b5b5cd590b55e31ec74845f0ae79fc548
palashbalpande <palashbalpande37@gmail.com>
2024-09-23 09:54:54 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -2,22 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res, next) => {
-  console.log("Handline One");
-  //res.send("Response One");
-  next();
-},
-  (req, res, next) => {
-    console.log("Handline Two");
-    //res.send("Response Two");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handline Three");
-    res.send("Response Three");
-  }
-); 
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.use("/user", userAuth, (req, res) => {
+  res.send("User Data sent");
+})
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("ALl Data sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("User Deleted");
+});
 
 app.listen(7777, () => {
-  console.log("Server listening on port 3000...");
+  console.log("Server listening on port 7777...");
 });
