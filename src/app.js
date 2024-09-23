ae726c7ef5c53b41aef713fab01a37e34a1823ef
palashbalpande <palashbalpande37@gmail.com>
2024-09-23 17:25:51 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,31 +1,48 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    // Log your error
-    res.status(500).send("Something went wrong");
-  }
+// app.post("/signup", async (req, res) => {
+//   //creating a new instance of User model
+//   try {
+//     const user = new User({
+//       firstName: "Palash",
+//       lastName: "Balpande",
+//       emailId: "pal@bal.com",
+//       password: "p@123",
+//     });
+//     await user.save();
+//     res.send("User created successfully");
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Error creating user");
+//   }
+// });
+
+app.use("/signup", async (req, res) => {
+  //creating a new instance of User model
+  const user = new User({
+    firstName: "Virat",
+    lastName: "Kohli",
+    emailId: "vir@koh.com",
+    password: "v@123",
+  });
+  try {
+    await user.save();
+    res.send("User created successfully");  
+  } catch (error) {
+    res.status(400).send("Error creating user" + error.message);
+  } 
 });
 
-app.use("/getUserData", (req, res) => {
-  //try {
-    throw new Error("User Data not sent");
-    res.send("User Data sent");
-  //} catch (error) {
-    //res.status(500).send("Some Error, Please Contact Suport team");
-  //}
-});
-
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    // Log your error
-    res.status(500).send("Something went wrong");
-  }
-});
-
-
-app.listen(7777, () => {
-  console.log("Server listening on port 7777...");
-});
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+    app.listen(7777, () => {
+      console.log("Server listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.err("Error connecting to MongoDB");
+  });
