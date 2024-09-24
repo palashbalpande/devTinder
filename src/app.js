09ddf16c08ee115cd7c067c6ba27a224fcd70b14
palashbalpande <palashbalpande37@gmail.com>
2024-09-24 08:35:38 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -3,37 +3,17 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-// app.post("/signup", async (req, res) => {
-//   //creating a new instance of User model
-//   try {
-//     const user = new User({
-//       firstName: "Palash",
-//       lastName: "Balpande",
-//       emailId: "pal@bal.com",
-//       password: "p@123",
-//     });
-//     await user.save();
-//     res.send("User created successfully");
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Error creating user");
-//   }
-// });
+app.use(express.json());
 
 app.use("/signup", async (req, res) => {
   //creating a new instance of User model
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailId: "vir@koh.com",
-    password: "v@123",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
-    res.send("User created successfully");  
+    res.send("User created successfully");
   } catch (error) {
     res.status(400).send("Error creating user" + error.message);
-  } 
+  }
 });
 
 connectDB()
