a794e3ba82436ae42b43fe16c547a233eb06f2a6
palashbalpande <palashbalpande37@gmail.com>
2024-09-25 17:26:53 +0530

    exploring validator library

--- src/app.js
+++ src/app.js
@@ -67,7 +67,7 @@ app.patch("/user/:userId", async (req, res) => {
   const data = req.body;
 
   try {
-    const ALLOWED_UPDATES = ["photoUrl", "about", "age", "skills"];
+    const ALLOWED_UPDATES = ["photoUrl", "about", "age", "gender", "skills"];
 
     const isUpdateAllowed = Object.keys(data).every((k) =>
       ALLOWED_UPDATES.includes(k)
--- src/models/user.js
+++ src/models/user.js
@@ -1,51 +1,70 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minlength: 4,
-    maxlength: 50,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 4,
+      maxlength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validator(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Password is not strong enough");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Invalid gender");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about the user",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: { 
-    type: String,
-  },
-  emailId: {
-    type: String,
-    lowercase: true,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  password: { 
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    min: 18,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      if(!["male", "female", "others"].includes(value)) {
-        throw new Error("Invalid gender");
-      }
-    }
-  },
-  photoUrl: {
-    type: String,
-    default: "https://geographyandyou.com/images/user-profile.png",
-  },
-  about: {
-    type: String,
-    default: "This is a default about the user",
-  },
-  skills: {
-    type: [String], 
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true,
-});
+);
 
 module.exports = mongoose.model("User", userSchema);
