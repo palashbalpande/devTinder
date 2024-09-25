041fa195fa274f4c3407b7c8b2598ec68b274f94
palashbalpande <palashbalpande37@gmail.com>
2024-09-25 07:52:30 +0530

    delete and update APIs

--- src/app.js
+++ src/app.js
@@ -47,6 +47,34 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+// Delete a user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    //const user = await User.findByIdAndDelete({ _id: userId });
+    const user = await User.findByIdAndDelete(userId); // { _id: userId }
+    if (!user) {
+      res.status(404).send("User not found");
+    } else res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+// Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  console.log(data);
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {returnDocument: 'before' });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Connected to MongoDB successfully");
--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
   },
-  lastName: {
+  lastName: { 
     type: String,
   },
   emailId: {
